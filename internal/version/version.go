aae04680c7b5b97d68f731d151f9f7b1e06db703
Joe <joe@complex64.com>
2022-01-19 21:01:55 +0100

    module's semantic version

---
+++ internal/version/version.go
@@ -0,0 +1,16 @@
+package version
+
+import (
+	"fmt"
+)
+
+const (
+	Major = 0
+	Minor = 0
+	Patch = 0
+)
+
+// String formats the semantic version string for this module.
+func String() string {
+	return fmt.Sprintf("v%d.%d.%d", Major, Minor, Patch)
+}
