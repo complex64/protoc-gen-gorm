c27b779aa15ddbb9b242b62e8cec6b2ac2501083
Joe <joe@complex64.com>
2022-01-20 12:38:22 +0100

    empty protoc plugin

---
+++ cmd/protoc-gen-gorm/internal_gengorm/main.go
@@ -0,0 +1,12 @@
+package internal_gengorm
+
+import (
+	"flag"
+
+	"google.golang.org/protobuf/compiler/protogen"
+)
+
+// GenerateFile does nothing yet.
+func GenerateFile(flags flag.FlagSet, plugin *protogen.Plugin, file *protogen.File) (*protogen.GeneratedFile, error) {
+	return nil, nil
+}
