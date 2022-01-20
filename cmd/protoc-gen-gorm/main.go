e2f8bb42d95fab14cc49dfdd4b5cde9d3bb68a28
Joe <joe@complex64.com>
2022-01-20 12:38:22 +0100

    empty protoc plugin

--- cmd/protoc-gen-gorm/main.go
+++ cmd/protoc-gen-gorm/main.go
@@ -1,4 +1,50 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	gengorm "github.com/complex64/protoc-gen-gorm/cmd/protoc-gen-gorm/internal_gengorm"
+	"github.com/complex64/protoc-gen-gorm/internal/version"
+	"google.golang.org/protobuf/compiler/protogen"
+)
+
+const (
+	docURL = "https://github.com/complex64/protoc-gen-gorm"
+)
+
+var (
+	showVersion = flag.Bool("version", false, "")
+	showHelp    = flag.Bool("help", false, "")
+)
+
 func main() {
+	flag.Parse()
+	if *showVersion {
+		fmt.Fprintf(os.Stdout, "%v %v\n", filepath.Base(os.Args[0]), version.String())
+		os.Exit(0)
+	}
+	if *showHelp {
+		fmt.Fprintf(os.Stdout, "Please see %s for usage information.\n", docURL)
+		os.Exit(0)
+	}
+
+	var (
+		flags flag.FlagSet
+	)
+	protogen.Options{
+		// Call `flags.Set(param, value)` for each from `--gorm_out=<param1>=<value1>,...`.
+		ParamFunc: flags.Set,
+	}.Run(func(plugin *protogen.Plugin) error {
+		for _, f := range plugin.Files {
+			if f.Generate {
+				if _, err := gengorm.GenerateFile(flags, plugin, f); err != nil {
+					return err
+				}
+			}
+		}
+		return nil
+	})
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/complex64/protoc-gen-gorm
 
 go 1.17
+
+require google.golang.org/protobuf v1.27.1
