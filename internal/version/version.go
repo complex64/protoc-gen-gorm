20894ba67a950c132f688031dd52c14d4ed968dc
complex64 <complex64@users.noreply.github.com>
2023-02-18 13:49:25 +0000

    update version to v2.0.2

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,7 +1,7 @@
 // Package version records versioning information about this module.
 package version
 
-var Version = "2.0.1"
+var Version = "2.0.2"
 
 // String formats the semantic version string for this module.
 func String() string {
